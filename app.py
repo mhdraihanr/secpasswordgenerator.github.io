0f61b1291842df149e3e883cd606fff759bb7506
mhdraihanr <raihanrafliansyah1234@gmail.com>
2024-12-05 16:23:29 +0700

    menambahkan  requirement dan procfile untuk railway

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import random
 import string
+import os
+
 
 app = Flask(__name__)
 
@@ -63,4 +65,5 @@ def check_strength():
     return render_template("check.html", strength=strength, password=password)
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
